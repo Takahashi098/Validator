aaa9a5723fa647f5b6fa28b15f02fd1da06d87ff
Takahashi098 <37509820+Takahashi098@users.noreply.github.com>
2018-07-19 22:28:39 +0700

    First commit

---
+++ src/sample/DateValidator.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample;
+
+/**
+ *
+ * @author DELL
+ */
+public class DateValidator {
+    
+    public boolean checkDateFormat (String date) {
+        
+        return date.matches("\\d{2}[/]\\d{2}[/]\\d{4}");
+    }
+    public boolean validateDate (String date) {
+        
+        if (checkDateFormat(date)) {
+            
+            int month = Integer.parseInt(date.substring(0, 2));
+            int day = Integer.parseInt(date.substring(3, 5));
+            int year = Integer.parseInt(date.substring(6, 10));
+
+            if (day < 0 || day > 31 || month < 0 || month > 12) {
+                return false;
+            }
+            else if (day > 30 && (month == 2 || month == 4 || month == 6 || month == 9 ||month == 11)) {
+                return false;
+            }
+            else if (day > 29 && month == 2) {
+                return false;
+            }
+            else if ((day == 29 && month == 2) && !((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)) {
+                return false;
+            } 
+        }
+        return true; 
+    }
+}
---
+++ test/DateValidatorTest.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import sample.DateValidator;
+
+/**
+ *
+ * @author DELL
+ */
+public class DateValidatorTest {
+    
+    @Test
+    public void test() {
+        
+        DateValidator tester = new DateValidator();
+        //Date Format test
+        assertEquals(false, tester.checkDateFormat(""));
+        assertEquals(false, tester.checkDateFormat("ab/cd/efgh"));
+        assertEquals(false, tester.checkDateFormat("15\12/2016"));
+        assertEquals(false, tester.checkDateFormat("1a/12/2016"));
+        assertEquals(false, tester.checkDateFormat("10/12/201"));
+        assertEquals(false, tester.checkDateFormat("3/1/2016"));
+        assertEquals(false, tester.checkDateFormat("31/1/2016"));
+        assertEquals(false, tester.checkDateFormat("3/31/2016"));
+        assertEquals(false, tester.checkDateFormat("30/31/16"));
+        assertEquals(true, tester.checkDateFormat("99/99/9999"));
+        //Date Valid test
+        assertEquals(false, tester.validateDate("99/99/9999"));
+        assertEquals(false, tester.validateDate("13/12/2016"));
+        assertEquals(false, tester.validateDate("11/31/2016"));
+        assertEquals(false, tester.validateDate("02/29/2018"));
+        assertEquals(true, tester.validateDate("02/29/2016"));
+        assertEquals(true, tester.validateDate("12/31/2016"));
+    }
+    
+}
