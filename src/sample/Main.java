cf3cc8ba8a5c72864a6472fc8f6fc3cf854440ea
Bao Bao <baolgse63067@fpt.edu.vn>
2018-07-19 23:37:10 +0700

    Commit 2

---
+++ src/sample/Main.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sample;
+
+/**
+ *
+ * @author DELL
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        String date = "12/12/2016";
+        DateValidator validator = new DateValidator();
+        if (validator.checkDateFormat(date)) {
+            if (validator.validateDate(date)) {
+                System.out.println(date + " is valid");
+            }
+            else {
+                System.out.println(date + "is invalid");
+            }
+        }
+        else {
+            System.out.println("Date format invalid");
+        }
+    }
+}
