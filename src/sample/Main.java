d32d8fa67038d1ca5c5dd7e63517b63a86aa19ff
Bao Bao <baolgse63067@fpt.edu.vn>
2018-07-23 07:45:28 +0700

    commit 5

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,6 +13,7 @@ public class Main {
     
     public static void main(String[] args) {
         
+        System.out.println("Đây là lần commit 5");
         System.out.println("Đây là lần commit 4");
         String date = "12/12/2016";
         DateValidator validator = new DateValidator();
